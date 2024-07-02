34baf2e640389fc82e6b8ce3aafebe416bb4fee8
alia-hc <ahcttino@gmail.com>
2024-07-02 14:16:02 -0600

    Create login screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,24 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:goal_minder/signup.dart';
+import 'package:goal_minder/new_event.dart';
+import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:convert';
 
+class Log extends StatefulWidget {
+  const Log({super.key});
 
-class LogIn extends StatefulWidget {
-  const LogIn({super.key});
-  
   @override
-  State<LogIn> createState() => LoginState();
+  State<Log> createState() => LoginState();
 }
 
-class LoginState extends State<LogIn> {
+class LoginState extends State<Log> {
+  TextEditingController userController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool notValid = false;
+
+  Future<void> login() async {
+  if (userController.text.isEmpty || passwordController.text.isEmpty) {
+    setState(() {
+      notValid = true;
+    });
+    return;
+  }
+
+  setState(() {
+    notValid = false;
+  });
+
+  var url = Uri.parse('http://10.0.2.2:3002/api/users/login');
+  var response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'username': userController.text,
+      'password': passwordController.text,
+    }),
+  );
+
+  
+
+  String errorMessage;
+  var responseJson = jsonDecode(response.body);
+  switch (response.statusCode) {
+    case 401:
+      errorMessage = responseJson['message'] ?? 'Contraseña incorrecta';
+      break;
+    case 404:
+      errorMessage = responseJson['message'] ?? 'Usuario no encontrado';
+      break;
+    case 400:
+      errorMessage = responseJson['message'] ?? 'Solicitud incorrecta';
+      break;
+    default:
+      errorMessage = responseJson['message'] ?? 'Error en el inicio';
+  
+    ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(errorMessage),
+          ),
+    );    
+  }
+
+   if (response.statusCode == 200) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NewEvent()),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Inicio de sesión correcto'),
+      ),
+    );
+  }
+
+
+}
+
+
   FocusNode myFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      backgroundColor: Color.fromRGBO(240, 242, 220, 1),
+      backgroundColor: const Color.fromRGBO(240, 242, 220, 1),
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 90.0),
-        children: <Widget> [
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 90.0),
+        children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -27,66 +98,104 @@ class LoginState extends State<LogIn> {
                 height: 180,
                 width: 180,
               ),
+              const SizedBox(height: 30),
               Padding(
                 padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 50.0),
                 child: Text(
                   'Login',
                   style: GoogleFonts.sansita(
-                    fontSize: 58,
+                    fontSize: 57,
                     fontStyle: FontStyle.italic,
+                    color: Color.fromARGB(255, 8, 8, 8),
                   ),
                 ),
               ),
-              
-              SizedBox(
-                width: 350, 
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Color.fromARGB(228, 199, 225, 221), 
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color.fromARGB(224, 2, 114, 129), width: 2),
-                        borderRadius: BorderRadius.circular(10.0)
-                      ),
-                      labelText: 'Username',
-                      labelStyle: TextStyle(
-                        color: Color.fromARGB(224, 2, 114, 129)
-                      )
-                    ),
-                )
-              ),
-
-              SizedBox(height: 20),
-
+              const SizedBox(height: 40),
               SizedBox(
                 width: 350,
-                  child: TextField(
+                child: TextField(
+                  controller: userController,
                   decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color.fromARGB(228, 199, 225, 221),
+                    errorStyle: TextStyle(color: Colors.redAccent),
+                    errorText: notValid ? "Campo vacío" : null,
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    labelText: 'Email',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(224, 2, 114, 129),
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Color.fromARGB(224, 2, 114, 129)),
                   ),
-                )
+                ),
               ),
-
-              SizedBox(height: 20),
-
+              const SizedBox(height: 20),
               SizedBox(
                 width: 350,
-                  child: TextField(
-                    obscureText: true,
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
                   decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color.fromARGB(228, 199, 225, 221),
+                    errorStyle: TextStyle(color: Colors.redAccent),
+                    errorText: notValid ? "Campo vacío" : null,
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(224, 2, 114, 129),
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(20.0)),
                     labelText: 'Password',
+                    labelStyle: TextStyle(color: Color.fromARGB(224, 2, 114, 129)),
                   ),
-                )
-              )
+                ),
+              ),
+              const SizedBox(height: 80),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Signup()),
+                  );
+                },
+                child: Text("Don't have an account yet? Register here"),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 217, 235, 82),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                onPressed: () {
+                  login();
+                },
+                child: Text(
+                  'Log in',
+                  style: GoogleFonts.kurale(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ),
             ],
           ),
         ],
